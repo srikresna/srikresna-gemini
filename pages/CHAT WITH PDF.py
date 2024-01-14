5f020eb7bcc46ead62f6ad5f92ea0924cc4a8720
srikresna <srikresna383@gmail.com>
2024-01-14 22:17:14 +0700

    chore: update outdated dependency

--- pages/CHAT WITH PDF.py
+++ pages/CHAT WITH PDF.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
