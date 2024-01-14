c3462f2ebee9c64187af82966b7b7df33b5efd5e
srikresna <srikresna383@gmail.com>
2024-01-14 21:18:50 +0700

    feat: add chat with pdf

---
+++ MAIN.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gemini AI",
+    page_icon=":gem:",
+)
+st.header("Welcome to Gemini AI 🤖")
+st.markdown("""
+    [![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+    Gemini AI is a platform that allows you to interact with AI models in a conversational manner.
+    You can ask questions to the models and they will respond to you.
+    You can also upload images and PDFs to the platform and ask questions about them.
+    The platform is powered by [Streamlit](https://streamlit.io) and hosted on [Streamlit Share](https://share.streamlit.io).
+    Contributions are welcome! [GitHub](https://github.com/srikresna/chatbot-gemini).
+            """)
+
+# logo github redirect ke repo
+st.markdown("""
+    [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/srikresna)
+    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/sri-kresna-maha-dewa-b0783a242/)
+    [![Medium](https://img.shields.io/badge/Medium-12100E?style=for-the-badge&logo=medium&logoColor=white)](https://medium.com/@srikresna.dewa)
+    [![Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/kresnanaa_)
+    [![Kaggle](https://img.shields.io/badge/Kaggle-20BEFF?style=for-the-badge&logo=kaggle&logoColor=white)](https://www.kaggle.com/srikresna)
+            """)
+
+         
+
---
+++ pages/CHAT WITH PDF.py
@@ -0,0 +1,149 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                   client=genai,
+                                   temperature=0.3,
+                                   )
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+def clear_chat_history():
+    # Delete the index file
+    if os.path.isfile("faiss_index/index.faiss"):
+        os.remove("faiss_index/index.faiss")
+
+    st.session_state.messages = [
+        {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
+
+    # Check if the index file exists before trying to load it
+    if not os.path.isfile("faiss_index/index.faiss"):
+        st.error("No index file found. Please upload a document first.")
+        return None
+
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    if new_db is not None:
+        docs = new_db.similarity_search(user_question)
+    else:
+        st.error("Failed to load the database.")
+        return None
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
+
+    print(response)
+    return response
+
+
+def main():
+    st.set_page_config(
+        page_title="Chat with PDF",
+        page_icon=":gem:",
+    )
+
+    with st.sidebar:
+        st.title("Chat setting")
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            if not pdf_docs:
+                st.error(
+                    "No PDF files uploaded. Please upload a file and try again.")
+            else:
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+
+    st.title("Chat with PDF 🤖")
+    st.write("""
+        Here you can chat with the AI model and upload PDFs to ask questions based on the PDFs.     
+             """
+             )
+    st.sidebar.button('Clear & Reset Chat', on_click=clear_chat_history)
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
+
+
+if __name__ == "__main__":
+    main()
